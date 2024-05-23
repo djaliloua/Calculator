5abccbece5c5ae46cd28a03cf9522ef1843bfe42
Abdou-Djalilou Ali <djaliloua@gmail.com>
2024-05-23 22:15:54 +0200

    Removed unneccessary codes

--- Calculator/MVVM/ViewModels/MainViewModel.cs
+++ Calculator/MVVM/ViewModels/MainViewModel.cs
@@ -4,8 +4,8 @@ using MVVM;
 
 namespace Calculator.MVVM.ViewModels
 {
-    public record Person(string name, string surname);
-    public class MainViewModel:Loadable<Person>
+    
+    public class MainViewModel:BaseViewModel
     {
         
         private bool _isBottomDrawerOpen;
@@ -20,25 +20,6 @@ namespace Calculator.MVVM.ViewModels
         {
             logger = _log;
             logger.LogInformation("MainViewModel started......");
-            _ = LoadItems();
-        }
-
-        public override async Task LoadItems()
-        {
-            await Task.Delay(1);
-
-            SetItems(Init());
-        }
-        private static IList<Person> Init()
-        {
-            return new List<Person>() 
-            { 
-                new Person("Ali", "Abdou"),
-                new Person("Ali", "Abdou"),
-                new Person("Ali", "Abdou"),
-                new Person("Ali", "Abdou"),
-                new Person("Ali", "Abdou"),
-            };
-        }
+        } 
     }
 }
