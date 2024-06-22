038e22b9a7b869e46fa25d26a867f3dfdc790fb9
Abdou-Djalilou Ali <djaliloua@gmail.com>
2024-06-22 14:55:08 +0200

    - save theme settings - Tooltip on ToggleButton

---
+++ Calculator/SettingsLayer/Implementations/ThemeSettings.cs
@@ -0,0 +1,18 @@
+﻿using Calculator.SettingsLayer.Abstractions;
+
+namespace Calculator.SettingsLayer.Implementations
+{
+    public class ThemeSettings : ISettings
+    {
+        public object GetParameter(string name)
+        {
+            return Properties.Settings.Default[name];
+        }
+
+        public void SetParameter(string name, object value)
+        {
+            Properties.Settings.Default[name] = value;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
